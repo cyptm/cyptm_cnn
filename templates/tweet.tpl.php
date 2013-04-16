fb1925e139095f2eaa4701e6a78e9214a68e7b22
U-Monordinateur\cyptm <cyptm@Monordinateur.(none)>
2013-04-16 09:24:04 -0400

    Linked creen name to twitter account

--- templates/tweet.tpl.php
+++ templates/tweet.tpl.php
@@ -12,7 +12,7 @@ $time_ago: The number of minutes ago tweet was posted
 <div class="tweet-wrapper">
 	<div class="tweet-picture "><img src="<?php print $picture; ?>" alt="<?php print $title; ?>" /></div>
 	<div class="tweet-content">
-		<h2><?php print $title; ?></h2><span class="screen-name faded-text"> <?php print '@'.$screen_name; ?></span>
+		<h2><a href="http://twitter.com/<?php print $screen_name; ?>"><?php print $title; ?></a></h2><span class="screen-name faded-text"> <?php print '@'.$screen_name; ?></span>
 		<span class="time-ago faded-text"> <?php print $time_ago; ?></span>
 		<p><?php print $status; ?></p>
 		<span class="faded-text"><?php print $RT; ?></span>
